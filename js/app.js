3b4d0f35278e1fbdbad3d5ea91a7e52bc489cbf9
oscarsete <oscarroth@gmail.com>
2022-08-31 15:28:10 -0300

    simulador-interactivo

---
+++ js/app.js
@@ -0,0 +1,18 @@
+//quiero que el carrito se muestren productos de tipos de piezas de cerámica, 
+//elegir categoría y luego sumar cantidad de esa categoría
+
+
+let option = prompt(`elegir una categoria:
+    1.Plato
+    2.Taza
+    3.Vaso
+    4.Fuente`);
+
+
+if (option === "1") {
+    console.log("plato");
+} else {
+    console.log("sin elegir");
+}
+
+
