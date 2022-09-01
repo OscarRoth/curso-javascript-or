67017474c1d1ca0c6f86da28459681c18fca5598
oscarsete <oscarroth@gmail.com>
2022-08-31 21:31:35 -0300

    simulador-interactivo

--- js/app.js
+++ js/app.js
@@ -2,17 +2,15 @@
 //elegir categoría y luego sumar cantidad de esa categoría
 
 
-let option = prompt(`elegir una categoria:
-    1.Plato
-    2.Taza
-    3.Vaso
-    4.Fuente`);
-
-
-if (option === "1") {
-    console.log("plato");
-} else {
-    console.log("sin elegir");
+let entrada = prompt(`Elegir un producto:
+1. Plato artesanal
+2. Taza de engobe
+3. Vaso para submarino
+4. Fuente de ensalada`);
+let productoIngresado = '';
+while (entrada != 'Voldemort') {
+    productoIngresado += entrada + "\n";
+    entrada = prompt("Ingresar nombre de producto");
 }
-
+console.log(productoIngresado);
 
