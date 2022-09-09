9e6f5df221bfb65d81b4e025031b4f7b43283ca2
oscarsete <oscarroth@gmail.com>
2022-09-09 14:58:31 -0300

    curso-javascript-or

--- js/app.js
+++ js/app.js
@@ -1,68 +1,86 @@
-//quiero que el carrito se muestren productos de tipos de piezas de cerámica, 
-//elegir categoría y luego sumar cantidad de esa categoría
-//me base en ejemplos vistos en clase y los del booklets nº3 y nº4
-//ya me salió lo que buscaba hacer,era ingresar dos tipos de valores por prompt y devolver por consola todos los ingresos y
-//un mensaje por cada tipo de ingreso.
+//Quiero que el carrito se muestren productos de tipos de piezas de cerámica, 
+//elegir el producto y luego sumar cantidad del producto
 
-let entrada = prompt(`Elegir el producto:
-1.Plato artesanal
-2.Taza de engobe
-3.Vaso para submarino
-4.Fuente de ensalada`);
 
-switch (entrada) {
-    case "1":
-        console.log("Plato artesanal");
-        break;
-    case "2":
-        console.log("Taza de engobe");
-        break;
-    case "3":
-        console.log("Vaso para submarino");
-        break;
-    case "4":
-        console.log("Fuente de ensalada");
-        break;
-    default:
-        console.log("No elegiste producto.");
-        break;
+/* simulador interactivo + clase 5, 6, 7 */
+
+const productos = [{
+    id: 1,
+    name: 'Plato artesanal',
+    price: 10,
+
+},
+
+{
+    id: 2,
+    name: 'Taza de engobe',
+    price: 15,
+
+},
+
+{
+    id: 3,
+    name: 'Vaso para submarino',
+    price: 20,
+
+},
+
+{
+    id: 4,
+    name: 'Fuente de ensalada',
+    price: 10,
+
+},
+
+
+]
+
+const product = (cantidad) => {
+
+    const arrayProducto = [];
+
+
+    for (let i = 0; i < productos.length; i++) {
+
+        arrayProducto.push({
+            id: productos[i].id,
+            nombre: productos[i].name,
+            'Precio Unitario': productos[i].price,
+            Cantidad: cantidad,
+            'Precio Total': productos[i].price * cantidad
+        })
+    };
+
+    return arrayProducto
 }
 
-console.log("Elegiste el producto: " + entrada);
 
-let entradaCant = prompt(`Elegir cantidad:
-1
-2
-3
-4
-5`);
+const platoArtesanal = product(2)[0]
+console.log('Plato Artesanal :', platoArtesanal);
 
+const tazaDeEngobe = product(4)[1]
+console.log('Taza de Engobe :', tazaDeEngobe);
 
+const vasoSubmarino = product(4)[1]
+console.log('Vaso para submarino :', vasoSubmarino);
 
-if (entradaCant === 1) {
-    console.log("Elegiste" + entradaCant);
-} else if (entradaCant === 2) {
-    console.log("Elegiste" + entradaCant);
-} else if (entradaCant === 3) {
-    console.log("Elegiste" + entradaCant);
-} else if (entradaCant === 4) {
-    console.log("Elegiste" + entradaCant);
-} else if (entradaCant === 5) {
-    console.log("Elegiste" + entradaCant);
-} else if (entradaCant.length === 0) {
-    console.log("Elige cantidad por favor.");
-};
-
-
-console.log("Elegiste: " + entradaCant + " unidades.");
+const fuenteEnsalada = product(4)[1]
+console.log('Fuente de ensalada :', fuenteEnsalada);
 
 
 
-/* Array  clase 6 */
+const li = document.createElement("li")
+const ul = document.getElementById("ul").appendChild(li).innerHTML = platoArtesanal.nombre
 
-//const objeto1 = { id: 2, producto: "Vaso de Engobe" };
-//const array = [objeto1, { id: 5, producto: "Plato artesanal" }]
-//array.push({ id: 7, producto: "" });
-//console.log(array);
 
-/* AFunciones de orden superior  clase 7 */
+
+
+// .innerHTML = '<li>Primer Producto</li>'
+// ul.appendChild(renderizado)
+
+// console.log(renderizado);
+
+// let agregado = document.createElement("h5");
+// console.log(agregado);
+// agregado.innerHTML = "<h5>Parrado de demostracion</h5>";
+// document.body.appendChild(agregado);
