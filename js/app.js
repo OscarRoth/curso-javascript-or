a6a6359e342bf4294dba2e314970c9f3beb92614
oscarsete <oscarroth@gmail.com>
2022-09-15 16:00:38 -0300

    curso-javascript-or

--- js/app.js
+++ js/app.js
@@ -2,7 +2,55 @@
 //elegir el producto y luego sumar cantidad del producto
 
 
-/* simulador interactivo + clase 5, 6, 7 */
+/* simulador interactivo*/
+
+
+
+//while (x <= 5) {
+//    valor = parseInt(prompt('Ingrese valor:'));
+//   suma = suma;
+//    x = x + 1;
+//}
+//console.log('La suma de los valores es ' + suma);
+
+
+let plato = 100;
+let vaso = 200;
+let taza = 300;
+let suma = 0;
+let datoIngresado;
+
+do {
+    datoIngresado = parseInt(prompt("¿Qué producto quiere comprar?: 1.Plato, 2.Vaso, 3.Taza, 4.salir de la compra"));
+    switch (datoIngresado) {
+        case 1:
+            // alert("elegiste 1")
+            suma = suma + plato
+            datoIngresado = parseInt(prompt("Ingresar un producto: 1.Plato, 2.Vaso, 3.Taza, 4.saliste"));
+            break;
+        case 2:
+            suma = suma + vaso
+            datoIngresado = parseInt(prompt("Ingresar un producto: 1.Plato, 2.Vaso, 3.Taza, 4.saliste"));
+            //alert("elegiste 2")
+            break;
+        case 3:
+            suma = suma + taza
+            datoIngresado = parseInt(prompt("Ingresar un producto: 1.Plato, 2.Vaso, 3.Taza, 4.saliste"));
+            //alert("elegiste 3")
+            break;
+        default:
+            alert("El numero ingresado no es correcto");
+            break;
+
+    }
+
+} while (datoIngresado != 4);
+
+alert("Compraste por un total de: " + suma);
+
+
+
+/* clase 5, 6, 7 */
 
 const productos = [{
     id: 1,
@@ -61,18 +109,16 @@ console.log('Plato Artesanal :', platoArtesanal);
 const tazaDeEngobe = product(4)[1]
 console.log('Taza de Engobe :', tazaDeEngobe);
 
-const vasoSubmarino = product(4)[1]
+const vasoSubmarino = product(4)[2]
 console.log('Vaso para submarino :', vasoSubmarino);
 
-const fuenteEnsalada = product(4)[1]
+const fuenteEnsalada = product(4)[3]
 console.log('Fuente de ensalada :', fuenteEnsalada);
 
 
-
+/* clase 8 DOM */
 const li = document.createElement("li")
 const ul = document.getElementById("ul").appendChild(li).innerHTML = platoArtesanal.nombre
-
-
 
 
 // .innerHTML = '<li>Primer Producto</li>'
